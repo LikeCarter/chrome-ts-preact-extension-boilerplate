56ff30b3e46f847263684b90856b3c57278c0554
Carter Sprigings <carter@sprigings.com>
2020-05-22 10:59:12 -0400

    Convert to preact.

--- src/ui/popup.tsx
+++ src/ui/popup.tsx
@@ -1,21 +1,17 @@
-import * as React from "react"
-import * as ReactDOM from "react-dom"
+import { h, Component, render } from "preact";
 
-import "../styles/popup.css"
+import "../styles/popup.css";
 
-class Hello extends React.Component {
-    render() {
-        return (
-            <div className="popup-padded">
-                <h1>{ chrome.i18n.getMessage("l10nHello") }</h1>
-            </div>
-        )
-    }
+class Hello extends Component {
+	render() {
+		return (
+			<div className="popup-padded">
+				<h1>{chrome.i18n.getMessage("l10nHello")}</h1>
+			</div>
+		);
+	}
 }
 
 // --------------
 
-ReactDOM.render(
-    <Hello />,
-    document.getElementById('root')
-)
+render(<Hello />, document.getElementById("root"));
